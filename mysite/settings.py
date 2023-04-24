bbd09e24ebedaafde25c69faa7062450a6eb2c04
wangleiz166 <wangleiz166@126.com>
2023-04-24 15:40:34 +0100

    fixbug

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Shopping.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
--- shopping_table/management/commands/shop_csv.py
+++ shopping_table/management/commands/shop_csv.py
@@ -1,4 +1,6 @@
 import csv
+from decimal import Decimal
+import decimal
 import os
 from pathlib import Path
 from django.db import models
@@ -6,13 +8,10 @@ from django.core.management.base import BaseCommand, CommandError
 from django.apps import apps
 import django
 
-
 from shopping_table.models import Alcohol
-
 
 class Command(BaseCommand):
     help = 'Load data from csv'
-
 
     def handle(self, *args, **options):
         apps.check_apps_ready()
@@ -22,21 +21,24 @@ class Command(BaseCommand):
 
         # open the file to read it into the database
         beer_dir = Path(__file__).resolve().parent.parent.parent.parent
-        with open(str(beer_dir)+'/disaster_table/ShoppingSource/Product_range.csv', newline='') as f:
+        with open(str(beer_dir)+'/shopping_table/ShoppingSource/Product_range.csv', encoding='utf-8', newline='') as f:
             reader =csv.reader(f, delimiter=",")
             next(reader)
             # skip the header line
 
             for row in reader:
                 print(row)
-                product_object = Alcohol.objects.create(
-
-                    product_name=row[3],
-                    product_id=row[1],
-                    product_country=row[6],
-                    product_price=row[4],
-                    product_size=row[7],
-                    product_ABV=row[8]
-                )
+                try:
+                    product_object = Alcohol.objects.create(
+                        product_id=row[0],
+                        product_name=row[2],
+                        product_price=int(float(row[3])) if row[3] else 0,
+                        product_country=row[5],
+                        product_size=Decimal(row[6]) if row[6] else Decimal('0'),
+                        product_ABV=Decimal(row[7]) if row[7] else Decimal('0'),
+                    )
+                except decimal.InvalidOperation:
+                    print(f"Failed to convert {row[6]} or {row[7]} to Decimal")
+                    continue
                 product_object.save()
                 print("data parsed successfully")
